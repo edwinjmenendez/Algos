0b87a96af439151f3a53ca8f04ffa8dd1eb5ca4f
Edwin <edwinjmenendez@gmail.com>
2020-11-05 13:14:51 -0800

    reverseInteger complete

--- reverseInteger.js
+++ reverseInteger.js
@@ -1,11 +1,18 @@
-const reverse = x => {
-  // given an integer we want to reverse it 
-  // turn the integer into a string so create a variable for it
-  let numToString = x
-  // if integer is negative, make it into a positive
-  if (numToString > -9 && numToString < 10) return x;  
-  // reverse the string's order
+// given an integer we want to reverse it 
+const reverseInt = x => {
+  // if x is a single digit value return x
+  if (x > -9 && x < 10) return x;
+  // create a variable if integer is negative, make it into a positive
+  let numToString = x;
+  if (x < 0) numToString *= -1;
+  // convert variable into string; split it as an array; reverse each el in array; join elements together as string
+  numToString = numToString.toString().split('').reverse().join('')
   // convert back to integer
+  numToString = parseInt(numToString)
   // if original value was negative, multiply by -1
+  if (x < 0) numToString *= -1;
+  // if numString exceeds 32 bit return 0
+  if (numToString > 2**31 - 1 || numToString < -(2**31)) return 0;
+  return numToString;
 }
-console.log(reverse(-1))
+console.log(reverseInt(-1534236469))
