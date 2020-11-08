c62cee28f8fe2bdc040354877ca84d2e7c5ae210
Edwin <edwinjmenendez@gmail.com>
2020-11-08 15:07:35 -0800

    removeDeplicates works; alt. solution

--- removeDuplicates.js
+++ removeDuplicates.js
@@ -14,9 +14,8 @@ const removeDuplicates = nums => {
     // if currEl is greater than nextEl return false
     if (nums[i] > nums[i + 1]) return 'false; not ascending';
     // if currentEl equals next el in array remove that currentEL from array (in place)
-    if (nums[i] === nums[i+1] || nums[i] === nums[i-1]) {
-      nums.splice(i, 1)
-      console.log(nums)
+    if (nums[i] === nums[i+1]) {
+      nums.splice(i, 1);
       i = i - 1;
     }
   }
@@ -24,4 +23,25 @@ const removeDuplicates = nums => {
   return nums.length;
 };
 
-console.log(removeDuplicates([0,0,1,1,1,2,2,3,3,3,3,3,4,4,4,4,4,4,4,5,5,5,5,5,5]));
+const removeDuplicates2 = nums => {
+  // if array length is less than two return length of array or if length > 3*10^4
+  if (nums.length < 2) return nums.length;
+  if (nums.length > 3*(10**4)) return 'false; length too big';
+  // iterate through array of numbers
+  let count = 0;
+  for (let i = 0; i < nums.length; i++) {
+    // each number must be in between 10^4 <= nums[i] <= 10^4
+    if (nums[i] < -(10**4) || nums[i] > 10**4) return 'false; number out of bounds';
+    // if currEl is greater than nextEl return false
+    if (nums[i] > nums[i + 1]) return 'false; not ascending';
+    // if currentEl equals next el in array remove that currentEL from array (in place)
+    if (nums[i] !== nums[count]) {
+      count++;
+      nums[count] = nums[i]
+    }
+  }
+  // return length of array
+  return count + 1;
+};
+
+console.log(removeDuplicates2([0,0,2,2,2,2,3,3,4,4,4,4,4,4]));
